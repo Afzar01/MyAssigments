0466288e4633015c9e9bd5cc465949883d6d3fae
Afzar01 <153655003+Afzar01@users.noreply.github.com>
2023-12-13 22:22:49 +0530

    Add files via upload
    day 2 
    with datatypes

---
+++ day2/Laptop.java
@@ -0,0 +1,30 @@
+package week1.day2;
+
+public class Laptop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String laptopName="Dell";
+		System.out.println(laptopName);
+		int laptopPrice=50000;
+		System.out.println("laptopPrice ="+laptopPrice);
+		char firstLetter='D';
+		System.out.println("firstLetter ="+firstLetter );
+		boolean isDamaged=false;
+		System.out.println("isDamaged ="+isDamaged);
+		float version= 10.522098f;
+		System.out.println("version ="+version);
+		long modelNumber=151134l;
+		System.out.println("modelNumber ="+modelNumber);
+		double embeddedControllerVersion = 255.255;
+		System.out.println("embedded control version ="+embeddedControllerVersion);
+		short charge=100;
+		System.out.println("Charge ="+charge);
+		byte ram= 8;
+		System.out.println("Ram ="+ram);
+		
+
+	}
+
+}
---
+++ day2/Student.java
@@ -0,0 +1,30 @@
+package week1.day2;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String studentName="Mohamed Afzar";
+		System.out.println("Student Name = "+studentName);
+		long studentMobileNumber=9994864057l;
+		System.out.println("Student mobile number = "+studentMobileNumber);
+		char studentInitial='Z';
+		System.out.println("Student initial = "+studentInitial);
+		boolean isStudentPresent=true;
+		System.out.println("Is student present = "+isStudentPresent); 
+		float studentHeightInInches=5.64f;
+		System.out.println("Student height in inches = "+studentHeightInInches);
+		double studentPercentage=80.25;
+		System.out.println("Student percentage = "+studentPercentage);
+		short studentWeight= 72;
+		System.out.println("Student Weight = "+studentWeight);
+		byte studentCgp=8;
+		System.out.println("Student Cgp = "+studentCgp);
+		int studentAge=22;
+		System.out.println("Student Age = "+studentAge);
+		
+
+	}
+
+}
